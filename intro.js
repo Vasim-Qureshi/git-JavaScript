7337a35032092ccb02fe491251e2f6d10d454dfc
vasim-qureshi <vasimqureshi2000@gmail.com>
2024-12-05 02:31:57 +0530

    add javascript tutorial

---
+++ intro.js
@@ -0,0 +1,5 @@
+document.getElementById("demo").innerHTML ="hello student";
+document.getElementById("demo").style.color="red";
+document.getElementById("demo").style.fontSize="50px"
+document.getElementById("demo").style.textAlign="center";
+document.body.style.backgroundColor="lightGreen";
