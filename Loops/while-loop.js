89ef0dc6f71879c47f80386b8aa540211b46cb10
vasim-qureshi <vasimqureshi2000@gmail.com>
2024-12-09 00:17:45 +0530

    add chessboard , loops and oprators folders

---
+++ Loops/while-loop.js
@@ -0,0 +1,7 @@
+var i = 2;
+while(i<=20){
+    console.log(i);
+    // document.getElementById("demo-1").innerHTML=i;
+    i=i+2;
+}
+// document.getElementById("demo-1").innerHTML="loop complited";
