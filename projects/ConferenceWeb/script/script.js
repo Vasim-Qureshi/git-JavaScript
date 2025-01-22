58a24880103a7dd5f7ee759db0cf56148dc7cb16
Vasim-Qureshi <163860877+Vasim-Qureshi@users.noreply.github.com>
2025-01-22 20:45:47 +0000

    add projects MyTuneMusic and ConferenceStylesWeb

---
+++ projects/ConferenceWeb/script/script.js
@@ -0,0 +1,5 @@
+// Interactive button action
+document.querySelector('.hero button').addEventListener('click', () => {
+    alert('Registration is open now!');
+  });
+  
---
+++ projects/MyTuneMusic/script/script.js
@@ -0,0 +1,5 @@
+// Example JS for future interactivity
+document.querySelector('.hero button').addEventListener('click', () => {
+    alert('Welcome to My Tunes!');
+  });
+  
