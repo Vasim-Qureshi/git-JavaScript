b6bf0424bd13a08d6976cc72c1e41e8f8896815e
Vasim-Qureshi <163860877+Vasim-Qureshi@users.noreply.github.com>
2025-01-23 18:59:03 +0000

    add fcc form

---
+++ projects/form/script/script.js
@@ -0,0 +1,6 @@
+// Form submission handling
+document.querySelector('#survey-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    alert('Thank you for submitting the survey!');
+  });
+  
